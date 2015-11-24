137a4dde83e6862135e54beb9ce9b364bda452a1
Daniel <daniel.fischer.dd@gmx.de>
2015-11-24 16:27:48 +0100

    jetz hab ich zweitens auch noch allein gemacht, brauchst mir auch dafür nich danken ;D

--- 3.Aufgabe/skelett/app.js
+++ 3.Aufgabe/skelett/app.js
@@ -22,6 +22,7 @@ app.use( express.static( path.join(__dirname, "public") ) );
 // Aufgabe 1
 var json_obj = [];
 
+
 var converter = new Converter({});
 
 converter.on("end_parsed", function (jsonArray) {
@@ -38,9 +39,56 @@ require("fs").createReadStream("./world_data.csv").pipe(converter);
 
 // Aufgabe 2
 
+app.get('/items', function (req, res) {
+    res.json(json_obj);
+});
+
+app.get('/items/:id', function (req, res) {
+    var id = req.params.id;
+
+    if(id > json_obj.length){
+        res.send("No such id " + id + " in database.");
+    }else if(id < 1) {
+        res.send("No such id " + id + " in database.");
+    }
+
+    var user = json_obj[id - 1];
+    res.json(user);
+});
 
 
+app.get('/items/:id1/:id2', function (req, res) {
+    var id1 = req.params.id1;
+    var id2 = req.params.id2;
+    var solution = [];
 
+    for( i = id1 - 1; i <= id2 - 1; i++){
+        console.log(i);
+
+        solution.push(json_obj[i]);
+    }
+    res.json(solution);
+});
+
+
+app.get('/properties', function (req, res) {
+
+    var keys = [];
+    for (key in json_obj[0]) {
+        keys.push(key);
+    }
+    res.json(keys);
+});
+
+app.get('/properties/:num', function (req, res) {
+    var num = req.params.num;
+    var keys = [];
+    for (key in json_obj[0]) {
+        keys.push(key);
+    }
+
+    res.json(keys[num]);
+});
 
 // DO NOT CHANGE!
 // bind server to port
