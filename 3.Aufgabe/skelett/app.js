789c72e2a77b10498894f60cd84d5bc37715c762
Daniel <daniel.fischer.dd@gmx.de>
2015-11-26 16:30:01 +0100

    und wieder einmal habe ich alles alleine gemacht.. langsam mach ich mir ne liste und stells dir in rechnung

--- 3.Aufgabe/skelett/app.js
+++ 3.Aufgabe/skelett/app.js
@@ -39,31 +39,31 @@ require("fs").createReadStream("./world_data.csv").pipe(converter);
 // Aufgabe 2
 //GET Calls
 
-// gibt alle Länder mit allen Properties zurück
+// gibt alle Lï¿½nder mit allen Properties zurï¿½ck
 app.get('/items', function (req, res) {
     res.json(json_obj);
 });
 
-// gibt ein Land mit id und allen Properties zurück, Wenn id nicht vorhanden: Status „No such id in database.“
+// gibt ein Land mit id und allen Properties zurï¿½ck, Wenn id nicht vorhanden: Status ï¿½No such id in database.ï¿½
 app.get('/items/:id', function (req, res) {
     var id = req.params.id;
-// wenn id größer als das oben erzeugte Array ist, gibt es die Meldung „No such id  in database. aus
+// wenn id grï¿½ï¿½er als das oben erzeugte Array ist, gibt es die Meldung ï¿½No such id  in database. aus
     if(id > json_obj.length){
         res.send("No such id " + id + " in database.");
-        // oder wenn id kleiner 1 ist, gibt es die Meldung auch „No such id in database. aus
+        // oder wenn id kleiner 1 ist, gibt es die Meldung auch ï¿½No such id in database. aus
     }else if(id < 1) {
         res.send("No such id " + id + " in database.");
     }
 
     var user = json_obj[id - 1];
-    // Ausgabe von der id mit den dazugehörigen Properties
+    // Ausgabe von der id mit den dazugehï¿½rigen Properties
     res.json(user);
 });
 
 
-// gibt alle Länder zwischen id1 und id2 mit allen Properties zurück, wenn Range nicht existiert: Status „Range not possible.“
+// gibt alle Lï¿½nder zwischen id1 und id2 mit allen Properties zurï¿½ck, wenn Range nicht existiert: Status ï¿½Range not possible.ï¿½
 app.get('/items/:id1/:id2', function (req, res) {
-    // Variablen für die 2 id's anlegen
+    // Variablen fï¿½r die 2 id's anlegen
     var id1 = req.params.id1;
     var id2 = req.params.id2;
     // erzeugen von ergebnis array
@@ -87,7 +87,7 @@ app.get('/items/:id1/:id2', function (req, res) {
     res.json(solution);
 });
 
-// gibt alle Properties zurück (id, name, birth_rate_per ...)
+// gibt alle Properties zurÃ¼ck (id, name, birth_rate_per ...)
 app.get('/properties', function (req, res) {
 
     // Array anlegen
@@ -100,8 +100,8 @@ app.get('/properties', function (req, res) {
 });
 
 
-// gibt Property mit der Nummer: num zurück, wenn num nicht vorhanden: Status: „No such property available.“
-app.get('/properties/:num', function (req, res) {
+// gibt Property mit der Nummer: num zurï¿½ck, wenn num nicht vorhanden: Status: ï¿½No such property available.ï¿½
+app.get('/properties/:num', function (req, res){
     var num = req.params.num;
     var keys = [];
 
@@ -120,7 +120,39 @@ app.get('/properties/:num', function (req, res) {
     res.json(keys[num]);
 });
 
+//POST-Calls
 
+app.post('/items', function(req, res){
+
+    var newItem = req.body;
+    json_obj.push(newItem);
+
+    res.send("Added Country " + req.body.name + " to list!");
+});
+
+
+app.delete('/items', function(req, res) {
+    var id = json_obj.length;
+    var name = json_obj[id - 1][1];
+    json_obj.pop();
+
+    res.send("Deleted las country: " + name + "!");
+});
+
+app.delete('/items/:id', function(req, res) {
+    var id = req.params.id;
+    success = new Boolean(false);
+    for(i = 0; i <= json_obj.length; i++){
+        if(json_obj[i][0] == id){
+            json_obj.splice(i,1);
+            success = true;
+            res.send("Item " + id + " deleted successfully.")
+        }
+    }
+    if(success == false){
+        res.send("No such id " + id + " in database.");
+    }
+});
 
 
 
