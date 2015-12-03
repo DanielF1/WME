fc1c19b67ee9ce37fab23ed9f085bcfa605db0e6
Daniel <daniel.fischer.dd@gmx.de>
2015-12-03 14:41:01 +0100

    just do iiiiiiiiiiit!!!!

--- 3.Aufgabe/skelett/public/assets/js/ajax.js
+++ 3.Aufgabe/skelett/public/assets/js/ajax.js
@@ -1,1 +1,62 @@
-alert("No ajax calls implemented ;)");
+$(document).ready(function(){
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/items",
+        async: true,
+        success: function(data) {
+
+            var tr=[];
+            for (var i = 0; i < data.length; i++) {
+                tr.push('<tr>');
+                tr.push("<td>" + data[i]["id"] + "</td>");
+                tr.push("<td>" + data[i]["name"] + "</td>");
+                tr.push("<td>" + data[i]["birth rate per 1000"] + "</td>");
+                tr.push("<td>" + data[i]["cell phones per 100"] + "</td>");
+                tr.push("<td>" + data[i]["children per woman"] + "</td>");
+                tr.push("<td>" + data[i]["electricity consumption per capita"] + "</td>");
+                tr.push("<td>" + data[i]["internet user per 100"] + "</td>");
+                tr.push('</tr>');
+            }
+            $('#table_body').append($(tr.join('')));
+
+        },
+        error: function(jqXHR, text, err) {
+            console.log(jqXHR);
+            console.log(text);
+            console.log(err);
+        }
+})});
+
+$('#add_submit').click(function(){
+
+    var i = $('#country_filter_id').val();
+
+    window.location.replace('/items/' + i);
+
+$.ajax({
+    type: "GET",
+    url: "http://localhost:3000/items/:id",
+    async: true,
+    success: function(data) {
+
+        var tr=[];
+        tr.push('<tr>');
+        tr.push("<td>" + data["id"] + "</td>");
+        tr.push("<td>" + data["name"] + "</td>");
+        tr.push("<td>" + data["birth rate per 1000"] + "</td>");
+        tr.push("<td>" + data["cell phones per 100"] + "</td>");
+        tr.push("<td>" + data["children per woman"] + "</td>");
+        tr.push("<td>" + data["electricity consumption per capita"] + "</td>");
+        tr.push("<td>" + data["internet user per 100"] + "</td>");
+        tr.push('</tr>');
+
+        $('#table_body').append($(tr.join('')));
+
+    },
+    error: function(jqXHR, text, err) {
+        console.log(jqXHR);
+        console.log(text);
+        console.log(err);
+    }
+})});
